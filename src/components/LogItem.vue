786d19f3be56c0f5b1728c4a6f1eb48d8effe9e5
dorara@work <satosinisemono@gmail.com>
2021-06-01 10:41:42 +0900

    mod: [log] 日付をフォーマット、曜日を表示

--- src/components/LogItem.vue
+++ src/components/LogItem.vue
@@ -1,15 +1,26 @@
 <template>
   <li>
-    <div class="log_date">{{ date }}</div>
+    <div class="log_date">{{ formatedDate(date) }}</div>
     <div class="log_percent">{{ percent }}%</div>
     <div class="log_memo">{{ memo }}</div>
   </li>
 </template>
 
 <script>
+import dayjs from 'dayjs'
 export default {
   name: 'LogItem',
-  props: ['date', 'percent', 'memo']
+  props: ['date', 'percent', 'memo'],
+  methods: {
+    // dateを受け取ってそれを 5/28（金）にする
+    formatedDate: function(date) {
+      const dayjsType = dayjs(date)
+      // 日付を日本語表記にするための配列
+      const weeks = ['日', '月', '火', '水', '木', '金', '土']
+      const formated = `${dayjsType.format('M/D')}（${weeks[dayjsType.day()]}）`
+      return formated
+    }
+  }
 }
 </script>
 
