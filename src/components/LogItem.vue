979feba97c6901357cfeda329acc0b9df4a70c5b
dorara@work <satosinisemono@gmail.com>
2021-06-14 11:24:50 +0900

    add: ログを編集機能(途中

--- src/components/LogItem.vue
+++ src/components/LogItem.vue
@@ -2,8 +2,18 @@
   <li>
     <div class="log_date">{{ formatedDate(date) }}</div>
     <div class="log_percent">{{ percent }}%</div>
+    <input type="text" ref="log_percent_edit" v-if="isEdit">
     <div class="log_memo">{{ memo }}</div>
-    <div class="log_btn_delete" @click="deleteItem(date)">削除</div>
+    <input type="text" ref="log_memo_edit" v-if="isEdit">
+
+    <div v-if="!isEdit">
+      <div class="log_btn" @click="isEdit=true">編集</div>
+      <div class="log_btn log_btn_delete" @click="deleteItem(date)">削除</div>
+    </div>
+    <div v-if="isEdit">
+      <div class="log_btn" @click="editItem(date)">保存</div>
+      <div class="log_btn" @click="isEdit=false">キャンセル</div>
+    </div>
   </li>
 </template>
 
@@ -13,6 +23,11 @@ export default {
   name: 'LogItem',
   props: ['date', 'percent', 'memo'],
   emits: ['delete'],
+  data() {
+    return {
+      isEdit: false
+    }
+  },
   methods: {
     // dateを受け取ってそれを 5/28（金）にする
     formatedDate: function(date) {
@@ -21,6 +36,11 @@ export default {
       const weeks = ['日', '月', '火', '水', '木', '金', '土']
       const formated = `${dayjsType.format('M/D')}（${weeks[dayjsType.day()]}）`
       return formated
+    },
+
+    editItem: function(date) {
+      alert(`${date}のデータを上書き保存します。`)
+      this.isEdit = false
     },
 
     deleteItem: function(date) {
@@ -50,11 +70,15 @@ li {
   flex: 1;
   padding-right: 50px;
 }
-.log_btn_delete {
+.log_btn {
   background-color: #444;
   font-size: 15px;
   color: #fff;
   padding: 5px 7px;
   border-radius: 20px;
+  cursor: pointer;
+}
+.log_btn_delete {
+  background-color: crimson;
 }
 </style>
