52db3482b1107bf77cc96d7b680a9168dae2fdea
dorara@work <satosinisemono@gmail.com>
2021-11-30 15:53:15 +0900

    tmp: env書き方修正2

--- src/env.js
+++ src/env.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: import.meta.env.VUE_APP_APIKEY,
-  appId: import.meta.env.VUE_APP_APPID,
-  authDomain: import.meta.env.VUE_APP_AUTHDOMAIN,
-  databaseURL: import.meta.env.VUE_APP_DATABASEURL,
-  measurementId: import.meta.env.VUE_APP_MEASUREMENTID,
-  messagingSenderId: import.meta.env.VUE_APP_MESSAGINGSENDERID,
-  projectId: import.meta.env.VUE_APP_PROJECTID,
-  storageBucket: import.meta.env.VUE_APP_STORAGEBUCKET
+  apiKey: import.meta.env.VITE_APP_APIKEY,
+  appId: import.meta.env.VITE_APP_APPID,
+  authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_APP_DATABASEURL,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENTID,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+  projectId: import.meta.env.VITE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET
 }
